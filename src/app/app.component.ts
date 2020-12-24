c36b78e88c8abc8ecac88c69598438a61d0fcfaf
zonghuang <1683766027@qq.com>
2020-12-24 21:45:44 +0800

    接口Interface

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
 import { Component } from '@angular/core';
 
+interface TopMenu {
+  title: string;
+  link: string;
+}
+
+interface AddFunc {
+  (x: number, y: number): number;
+}
+
+interface Dict {
+  [key: string]: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  tabs = ['热门', '男装', '手机'];
+  title = '拼多多';
+  menus: TopMenu[] = [
+    {
+      title: '热门',
+      link: ''
+    }
+  ];
+
+  constructor() {
+    this.test();
+  }
+
+  add: AddFunc = (x, y) => x + y;
+
+  dict: Dict = {
+    a: '1',
+    b: '2'
+  }
+
+  test() {
+    console.log(this.dict['a']);
+  }
+
 }
