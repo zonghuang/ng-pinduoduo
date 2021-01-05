f3feb5564ffda290260ac50ae4229ba0ea348f48
zonghuang <1683766027@qq.com>
2021-01-05 22:01:49 +0800

    1. *ngIf指令 2. 组件的输入和输出属性

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TopMenu } from './components';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +8,97 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = '拼多多';
+  topMenus: TopMenu[] = [
+    {
+      id: 1,
+      title: '热门',
+      link: 'hot',
+    },
+    {
+      id: 2,
+      title: '男装',
+      link: 'men',
+    },
+    {
+      id: 3,
+      title: '百货',
+      link: 'department',
+    },
+    {
+      id: 4,
+      title: '运动',
+      link: 'sports',
+    },
+    {
+      id: 5,
+      title: '手机',
+      link: 'mobile',
+    },
+    {
+      id: 6,
+      title: '家纺',
+      link: 'textile',
+    },
+    {
+      id: 7,
+      title: '食品',
+      link: 'food',
+    },
+    {
+      id: 8,
+      title: '电器',
+      link: 'appliance',
+    },
+    {
+      id: 9,
+      title: '鞋包',
+      link: 'shoes',
+    },
+    {
+      id: 10,
+      title: '汽车',
+      link: 'cars',
+    },
+    {
+      id: 11,
+      title: '水果',
+      link: 'fruits',
+    },
+    {
+      id: 12,
+      title: '电脑',
+      link: 'computers',
+    },
+    {
+      id: 13,
+      title: '内衣',
+      link: 'underwears',
+    },
+    {
+      id: 14,
+      title: '家装',
+      link: 'home',
+    },
+    {
+      id: 15,
+      title: '母婴',
+      link: 'baby',
+    },
+    {
+      id: 16,
+      title: '美妆',
+      link: 'makeup',
+    },
+    {
+      id: 17,
+      title: '家具',
+      link: 'furnitures',
+    },
+  ];
 
   constructor() {}
+
+  handleTabSelected(topMenu: TopMenu) {
+    console.log(topMenu);
+  }
 }
--- src/app/components/scrollable-tab/scrollable-tab.component.ts
+++ src/app/components/scrollable-tab/scrollable-tab.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
-interface TopMenu {
+export interface TopMenu {
   id: number;
   title: string;
   link: string;
@@ -13,93 +13,8 @@ interface TopMenu {
 })
 export class ScrollableTabComponent implements OnInit {
   selectedIndex = -1;
-  menus: TopMenu[] = [
-    {
-      id: 1,
-      title: '热门',
-      link: 'hot',
-    },
-    {
-      id: 2,
-      title: '男装',
-      link: 'men',
-    },
-    {
-      id: 3,
-      title: '百货',
-      link: 'department',
-    },
-    {
-      id: 4,
-      title: '运动',
-      link: 'sports',
-    },
-    {
-      id: 5,
-      title: '手机',
-      link: 'mobile',
-    },
-    {
-      id: 6,
-      title: '家纺',
-      link: 'textile',
-    },
-    {
-      id: 7,
-      title: '食品',
-      link: 'food',
-    },
-    {
-      id: 8,
-      title: '电器',
-      link: 'appliance',
-    },
-    {
-      id: 9,
-      title: '鞋包',
-      link: 'shoes',
-    },
-    {
-      id: 10,
-      title: '汽车',
-      link: 'cars',
-    },
-    {
-      id: 11,
-      title: '水果',
-      link: 'fruits',
-    },
-    {
-      id: 12,
-      title: '电脑',
-      link: 'computers',
-    },
-    {
-      id: 13,
-      title: '内衣',
-      link: 'underwears',
-    },
-    {
-      id: 14,
-      title: '家装',
-      link: 'home',
-    },
-    {
-      id: 15,
-      title: '母婴',
-      link: 'baby',
-    },
-    {
-      id: 16,
-      title: '美妆',
-      link: 'makeup',
-    },
-    {
-      id: 17,
-      title: '家具',
-      link: 'furnitures',
-    },
-  ];
+  @Input() menus: TopMenu[] = [];
+  @Output() tabSelected = new EventEmitter();
 
   constructor() {}
 
@@ -107,5 +22,6 @@ export class ScrollableTabComponent implements OnInit {
 
   handleSelection(index: number) {
     this.selectedIndex = index;
+    this.tabSelected.emit(this.menus[this.selectedIndex]);
   }
 }
