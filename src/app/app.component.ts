74bc3d2993eb6199182cce7b161067f10238b746
zonghuang <1683766027@qq.com>
2021-01-05 20:30:05 +0800

    封装第一个组件（ScrollableTab组件）

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,5 @@
 import { Component } from '@angular/core';
 
-interface TopMenu {
-  id: number;
-  title: string;
-  link: string;
-}
-
-interface AddFunc {
-  (x: number, y: number): number;
-}
-
-interface Dict {
-  [key: string]: string;
-}
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -21,101 +7,6 @@ interface Dict {
 })
 export class AppComponent {
   title = '拼多多';
-  selectedIndex = -1;
-  menus: TopMenu[] = [
-    {
-      id: 1,
-      title: '热门',
-      link: 'hot',
-    },
-    {
-      id: 2,
-      title: '男装',
-      link: 'men',
-    },
-    {
-      id: 3,
-      title: '百货',
-      link: 'department',
-    },
-    {
-      id: 4,
-      title: '运动',
-      link: 'sports',
-    },
-    {
-      id: 5,
-      title: '手机',
-      link: 'mobile',
-    },
-    {
-      id: 6,
-      title: '家纺',
-      link: 'textile',
-    },
-    {
-      id: 7,
-      title: '食品',
-      link: 'food',
-    },
-    {
-      id: 8,
-      title: '电器',
-      link: 'appliance',
-    },
-    {
-      id: 9,
-      title: '鞋包',
-      link: 'shoes',
-    },
-    {
-      id: 10,
-      title: '汽车',
-      link: 'cars',
-    },
-    {
-      id: 11,
-      title: '水果',
-      link: 'fruits',
-    },
-    {
-      id: 12,
-      title: '电脑',
-      link: 'computers',
-    },
-    {
-      id: 13,
-      title: '内衣',
-      link: 'underwears',
-    },
-    {
-      id: 14,
-      title: '家装',
-      link: 'home',
-    },
-    {
-      id: 15,
-      title: '母婴',
-      link: 'baby',
-    },
-    {
-      id: 16,
-      title: '美妆',
-      link: 'makeup',
-    },
-    {
-      id: 17,
-      title: '家具',
-      link: 'furnitures',
-    },
-  ];
 
-  constructor() {
-
-  }
-
-  handleSelection(index: number) {
-    this.selectedIndex = index;
-  }
-
+  constructor() {}
 }
