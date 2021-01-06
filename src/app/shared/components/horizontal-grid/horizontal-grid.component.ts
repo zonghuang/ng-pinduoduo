389f97c739404cd1716b64e2d6a23283dad44007
zonghuang <1683766027@qq.com>
2021-01-06 22:21:43 +0800

    自定义2个装饰器：@Emoji() 和 @Confirmable()

--- src/app/shared/components/horizontal-grid/horizontal-grid.component.ts
+++ src/app/shared/components/horizontal-grid/horizontal-grid.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Confirmable, Emoji } from '../../decorators';
 
 @Component({
   selector: 'app-horizontal-grid',
@@ -9,6 +10,7 @@ export class HorizontalGridComponent implements OnInit {
   // username = '';
   private _username = '';
   @Output() usernameChange = new EventEmitter();
+  @Emoji() result = 'Hello';
 
   constructor() {}
 
@@ -22,5 +24,10 @@ export class HorizontalGridComponent implements OnInit {
   public set username(value: string) {
     this._username = value;
     this.usernameChange.emit(value);
+  }
+
+  @Confirmable('您确认要执行吗？')
+  handleClick() {
+    console.log('点击已执行');
   }
 }
