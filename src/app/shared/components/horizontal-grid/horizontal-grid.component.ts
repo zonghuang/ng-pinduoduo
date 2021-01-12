8be8ad29548f7796310b58035f92a210ca3c482b
zonghuang <1683766027@qq.com>
2021-01-12 21:44:36 +0800

    1.投影组件 2.频道滚动栏

--- src/app/shared/components/horizontal-grid/horizontal-grid.component.ts
+++ src/app/shared/components/horizontal-grid/horizontal-grid.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit} from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 export interface Channel {
   id: number;
@@ -12,9 +12,29 @@ export interface Channel {
   styleUrls: ['./horizontal-grid.component.scss'],
 })
 export class HorizontalGridComponent implements OnInit {
+  @Input() cols = 8;
+  @Input() displayCols = 5;
+  sliderMargin = '0';
 
   constructor() {}
 
   ngOnInit() {}
 
+  public get scrollable(): boolean {
+    return this.cols > this.displayCols;
+  }
+
+  public get templateRows(): string {
+    return `minmax(auto, max-content)`;
+  }
+
+  public get templateColumns(): string {
+    return `repeat(${this.cols}, calc((100vw -
+      ${this.displayCols * 0.4}rem) / ${this.displayCols}))`;
+  }
+  handleScroll(ev) {
+    this.sliderMargin = `0 ${
+      (100 * ev.target.scrollLeft) / ev.target.scrollWidth
+    }%`;
+  }
 }
